0b5aafaedd02f2fc68e0d3496ee962b4b373c093
Valentyn Herasymenko <valikgera2002@gmail.com>
2023-02-11 17:48:24 +0200

    remove one const

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,6 @@ import ContactForm from './ContactForm/ContactForm';
 export default function App() {
   const contacts = useSelector(state => state.contacts);
 
-  const isShow = contacts.length > 0;
 
   return (
     <div
